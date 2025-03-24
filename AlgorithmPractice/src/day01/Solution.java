111b175ce774bdd639f23df17017d789c4762a95
sagejeong999@gmail.com <sagejeong999@gmail.com>
2025-03-24 21:27:27 +0900

    Day 01 - 프로그래머스 문제 풀이

---
+++ AlgorithmPractice/src/day01/Solution.java
@@ -0,0 +1,11 @@
+package day01;
+import java.util.Scanner;
+public class Solution {
+	    public static void main(String[] args) {
+	        String message = "Let's go!"; //Let's go! , 문자열이기에 큰따옴표 안에 넣음
+	        
+	        System.out.println("3\n2\n1"); // 3,2,1이 각각 한줄에 하나씩이여서 줄바꿔주는 \n 삽입
+	        System.out.println(message); // 주어진 문제에서 message가 마지막에 출력되야하므로 숫자 밑 삽입
+	    }
+	}
+
